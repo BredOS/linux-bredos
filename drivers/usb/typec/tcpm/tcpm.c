armbian/linux-rockchip

usb: typec: tcpm: add devicetree property faster-pd-negotiation to make pd negotiation faster

drivers/usb/typec/tcpm/tcpm.c
@@ -486,6 +486,7 @@ struct tcpm_port {
 	int logbuffer_tail;
 	u8 *logbuffer[LOG_BUFFER_ENTRIES];
 #endif
+	bool faster_pd_negotiation;
 };
 
 struct pd_rx_event {
@@ -4159,7 +4160,7 @@ static void run_state_machine(struct tcpm_port *port)
 		break;
 	case SNK_ATTACH_WAIT:
 		port->debouncing = true;
-		timer_val_msecs = PD_T_CC_DEBOUNCE;
+		timer_val_msecs = port->faster_pd_negotiation ? 100 : PD_T_CC_DEBOUNCE;
 		trace_android_vh_typec_tcpm_get_timer(tcpm_states[SNK_ATTACH_WAIT],
 						      CC_DEBOUNCE, &timer_val_msecs);
 		if ((port->cc1 == TYPEC_CC_OPEN &&
@@ -4317,7 +4318,7 @@ static void run_state_machine(struct tcpm_port *port)
 		if (port->vbus_never_low) {
 			port->vbus_never_low = false;
 			tcpm_set_state(port, SNK_SOFT_RESET,
-				       timer_val_msecs);
+				       port->faster_pd_negotiation ? 100 : timer_val_msecs);
 		} else {
 			tcpm_set_state(port, hard_reset_state(port),
 				       timer_val_msecs);
@@ -4874,7 +4875,7 @@ static void run_state_machine(struct tcpm_port *port)
 			       PD_T_ERROR_RECOVERY);
 		break;
 	case PORT_RESET_WAIT_OFF:
-		timer_val_msecs = PD_T_PS_SOURCE_OFF;
+		timer_val_msecs = port->faster_pd_negotiation ? 100 : PD_T_PS_SOURCE_OFF;
 		trace_android_vh_typec_tcpm_get_timer(tcpm_states[PORT_RESET_WAIT_OFF],
 						      SOURCE_OFF, &timer_val_msecs);
 		tcpm_set_state(port,
@@ -6116,6 +6117,8 @@ sink:
 	port->operating_snk_mw = mw / 1000;
 
 	port->self_powered = fwnode_property_read_bool(fwnode, "self-powered");
+
+	port->faster_pd_negotiation = fwnode_property_read_bool(fwnode, "faster-pd-negotiation");
 
 	/* FRS can only be supported byb DRP ports */
 	if (port->port_type == TYPEC_PORT_DRP) {

BredOS/linux-bredos

drivers/usb/typec/tcpm/tcpm.c
@@ -493,6 +493,7 @@ struct tcpm_port {
 	int logbuffer_tail;
 	u8 *logbuffer[LOG_BUFFER_ENTRIES];
 #endif
+	bool faster_pd_negotiation;
 };
 
 struct pd_rx_event {
@@ -4296,10 +4297,10 @@ static void run_state_machine(struct tcpm_port *port)
 		    (port->cc1 != TYPEC_CC_OPEN &&
 		     port->cc2 == TYPEC_CC_OPEN))
 			tcpm_set_state(port, SNK_DEBOUNCED,
-				       PD_T_CC_DEBOUNCE);
+				       port->faster_pd_negotiation ? 100 : PD_T_CC_DEBOUNCE);
 		else if (tcpm_port_is_disconnected(port))
 			tcpm_set_state(port, SNK_UNATTACHED,
-				       PD_T_PD_DEBOUNCE);
+				       port->faster_pd_negotiation ? 100 : PD_T_CC_DEBOUNCE);
 		else if (tcpm_port_is_sink(port))
 			tcpm_set_state(port, SNK_DEBOUNCED, 0);
 		break;
@@ -4434,7 +4435,7 @@ static void run_state_machine(struct tcpm_port *port)
 		if (port->vbus_never_low) {
 			port->vbus_never_low = false;
 			tcpm_set_state(port, SNK_SOFT_RESET,
-				       PD_T_SINK_WAIT_CAP);
+				       port->faster_pd_negotiation ? 100 : PD_T_SINK_WAIT_CAP);
 		} else {
 			tcpm_set_state(port, hard_reset_state(port),
 				       PD_T_SINK_WAIT_CAP);
@@ -4983,7 +4984,7 @@ static void run_state_machine(struct tcpm_port *port)
 	case PORT_RESET_WAIT_OFF:
 		tcpm_set_state(port,
 			       tcpm_default_state(port),
-			       port->vbus_present ? PD_T_PS_SOURCE_OFF : 0);
+			       port->vbus_present ? port->faster_pd_negotiation ? 100 : PD_T_PS_SOURCE_OFF : 0);
 		break;
 
 	/* AMS intermediate state */
@@ -6306,6 +6307,8 @@ sink:
 	if (fwnode_property_read_u32(fwnode, "op-sink-microwatt", &mw) < 0)
 		return -EINVAL;
 	port->operating_snk_mw = mw / 1000;
+
+	port->faster_pd_negotiation = fwnode_property_read_bool(fwnode, "faster-pd-negotiation");
 
 	/* FRS can only be supported by DRP ports */
 	if (port->port_type == TYPEC_PORT_DRP) {